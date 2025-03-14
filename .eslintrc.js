f8b52f9cbcdcb243cc21f5167338a365c052f8fc
jarmo-productory <jarmo@productory.eu>
2025-03-14 19:34:35 +0200

    Simplify configuration: consolidate Next.js config files and relax ESLint/TypeScript checks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,29 @@
 module.exports = {
-  extends: [
-    "next/core-web-vitals"
-  ],
+  extends: ['next/core-web-vitals'],
   rules: {
-    // Disable TypeScript strict rules
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/no-unnecessary-condition": "off",
-    
+    // Disable TypeScript strict rules for easier development
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+
     // Disable React strict rules
-    "react-hooks/rules-of-hooks": "warn",
-    "react-hooks/exhaustive-deps": "off",
-    "react/no-unescaped-entities": "off",
-    "react/jsx-no-comment-textnodes": "off",
-    
-    // Disable import rules
-    "import/no-duplicates": "off",
-    "import/order": "off",
-    "import/no-cycle": "off",
-    "import/no-self-import": "off"
+    'react-hooks/rules-of-hooks': 'warn',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/no-unescaped-entities': 'off',
   },
   ignorePatterns: [
-    "node_modules/",
-    ".next/",
-    "out/",
-    "build/",
-    "public/",
-    "**/*.test.ts",
-    "**/*.test.tsx",
-    "**/*.spec.ts",
-    "**/*.spec.tsx",
-    "**/__tests__/",
-    "**/__mocks__/",
-    "scripts/",
-    "lib/utils/"
-  ]
-}; 
+    'node_modules/',
+    '.next/',
+    'out/',
+    'build/',
+    'public/',
+    '**/*.test.ts',
+    '**/*.test.tsx',
+    '**/*.spec.ts',
+    '**/*.spec.tsx',
+    '**/__tests__/',
+    '**/__mocks__/',
+    'scripts/',
+    'lib/utils/',
+  ],
+};
--- next.config.js
+++ next.config.js
@@ -1,20 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ... your existing config ...
+  // Security headers from next.config.ts
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY',
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'strict-origin-when-cross-origin',
+        },
+        {
+          key: 'X-XSS-Protection',
+          value: '1; mode=block',
+        },
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=31536000; includeSubDomains',
+        },
+      ],
+    },
+  ],
+
+  // Webpack configuration
   webpack: (config, { isServer }) => {
-    config.ignoreWarnings = [
-      { module: /node_modules\/punycode/ }
-    ];
+    config.ignoreWarnings = [{ module: /node_modules\/punycode/ }];
     return config;
   },
+
   // Disable ESLint during builds to get the pipeline working
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   // Disable TypeScript errors during builds
   typescript: {
     ignoreBuildErrors: true,
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
